90b3c8371fbdb2870cdec86eba9deffbb3d59575
Hououin Kyouma <inwazzin@gmail.com>
2024-01-22 01:39:59 +0100

    latent manipulation

--- demo/utils/utils.py
+++ demo/utils/utils.py
@@ -8,6 +8,7 @@ def show_images(*images: Tensor):
     _, axes = plt.subplots(1, len(images), figsize=(16, 8))
     for i, image in enumerate(images):
       grid = vutils.make_grid(image, padding=2, normalize=True)
+      axes[i].axis('off')
       axes[i].imshow(np.transpose(grid.cpu().detach().numpy(), (1, 2, 0)))
   else:
     grid = vutils.make_grid(images[0], padding=2, normalize=True)
--- src/mod/models/classifier.py
+++ src/mod/models/classifier.py
@@ -1,25 +1,50 @@
-import numpy as np
-from torch import nn, Tensor
+from typing import Optional, Mapping, Any
 
-def initialize_weights(layer: nn.Module):
-  if isinstance(layer, nn.Linear):
-    nn.init.xavier_uniform_(layer.weight.data)
-    if layer.bias is not None:
-      nn.init.constant_(layer.bias.data, 0)
+from torch import nn
+
+def initialize_weights(layer):
+  if isinstance(layer, nn.Conv2d):
+    nn.init.kaiming_normal_(layer.weight.data, mode='fan_out', nonlinearity='relu')
+  elif isinstance(layer, nn.BatchNorm2d):
+    nn.init.constant_(layer.weight, 1)
+    nn.init.constant_(layer.bias, 0)
+  elif isinstance(layer, nn.Linear):
+    nn.init.normal_(layer.weight, 0, 0.01)
+    nn.init.constant_(layer.bias, 0)
 
 class Classifier(nn.Module):
-  def __init__(self, img_shape, num_traits):
+  def __init__(
+      self,
+      image_shape: tuple[int, int, int],
+      trait_count: int,
+      *,
+      with_weights: Optional[Mapping[str, Any]] = None,
+      with_init: bool = False
+  ):
     super(Classifier, self).__init__()
-    self.img_shape = img_shape
-    self.num_traits = num_traits
-
+    (channel_count, width, height) = image_shape
     self.model = nn.Sequential(
-      nn.Linear(int(np.prod(img_shape)), 512),
-      nn.ReLU(),
-      nn.Linear(512, 256),
-      nn.ReLU(),
-      nn.Linear(256, num_traits)
+      nn.Conv2d(channel_count, 32, 3, 1, 1),
+      nn.ReLU(True),
+      nn.MaxPool2d(2, 2),
+      nn.Conv2d(32, 64, 3, 1, 1),
+      nn.ReLU(True),
+      nn.MaxPool2d(2, 2),
+      nn.Conv2d(64, 128, 3, 1, 1),
+      nn.ReLU(True),
+      nn.MaxPool2d(2, 2),
+      nn.Flatten(),
+      nn.Linear(128 * (width // 8) * (height // 8), 512),
+      nn.ReLU(True),
+      nn.Dropout(0.5),
+      nn.Linear(512, trait_count),
+      nn.Sigmoid()
     )
 
+    if with_init:
+      self.apply(initialize_weights)
+    if with_weights:
+      self.load_state_dict(with_weights)
+
   def forward(self, x):
-    return self.model(x.view(x.size(0), -1))
+    return self.model(x)
