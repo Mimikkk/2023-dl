6fa1873692b5fa59193b73a656ca36bc58363672
Hououin Kyouma <inwazzin@gmail.com>
2024-01-24 00:17:20 +0100

    refactor

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from typing import Optional
+from typing import Optional, Callable
 
+print('Importing streamlit...')
 import streamlit as st
+print('Importing torch...')
 import torch.nn.parallel
 import torch.utils.data
 import matplotlib.animation as animation
@@ -13,16 +15,27 @@ from torchvision.transforms import transforms
 from src.mod.datasets import CelebA
 from src.mod.datasets.utils import create_latent_vectors
 from src.mod.models import Classifier, Generator, LatentSpaceManipulator
+print('Importing done.')
 
-def main():
-  ModelsDirectory = "demo/"
-  # >= threshold -> feature is treated present
-  FeatureThreshold = .4
-  NoFeature = "-"
-  LatentVectorSize = 100
 
-  device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+@st.cache_resource
+def load_device() -> torch.device:
+  print('Loading device...')
+  return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+@st.cache_resource
+def load_classifier(device: torch.device) -> Classifier:
+  print('Loading classifier...')
+  return Classifier((3, 64, 64), 40, with_weights=torch.load('demo/c.run_three.pt')).to(device)
+
+@st.cache_resource
+def load_generator(device: torch.device) -> Generator:
+  print('Loading generator...')
+  return Generator(100, 64, 3, with_weights=torch.load('demo/g.run_three.pt')).to(device)
+
+@st.cache_resource
+def load_dataset() -> CelebA:
+  print('Loading dataset...')
   transform = transforms.Compose([
     transforms.Resize(64),
     transforms.CenterCrop(64),
@@ -30,129 +43,130 @@ def main():
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
   ])
 
-  generator = Generator(
-    LatentVectorSize,
-    64,
-    3,
-    with_weights=torch.load(ModelsDirectory + 'g.run_three.pt', map_location=torch.device(device))
-  )
-  classifier = Classifier(
-    (3, 64, 64),
-    40,
-    with_weights=torch.load(ModelsDirectory + 'c.run_three.pt', map_location=torch.device(device))
-  )
-
-  dataset = CelebA(
+  return CelebA(
     dataset_path='resources/datasets/celeba',
     image_directory='img_align_celeba',
     annotations_directory='annotations',
     image_transform=transform
   )
 
-  def generate_image(name_postfix: str = '', noise: Optional[torch.Tensor] = None):
-    if not name_postfix or noise is None:
-      noise = create_latent_vectors(1, LatentVectorSize, device)
+@st.cache_resource
+def load_manipulator(generator: Generator, classifier: Classifier, device: torch.device) -> LatentSpaceManipulator:
+  print('Loading manipulator...')
+  return LatentSpaceManipulator(generator, classifier, device)
 
-    noise.to(device)
-    generator.to(device)
-    classifier.to(device)
+def array_from_torch(tensor: torch.Tensor) -> np.ndarray:
+  return np.transpose(vutils.make_grid(tensor, padding=2, normalize=True).cpu(), (1, 2, 0)).numpy()
 
-    generated = generator(noise)
-    features = classifier(generated).cpu().detach().numpy()[0]
+def main():
+  FeatureThreshold = .4
+  LatentVectorSize = 100
 
-    st.session_state[f'latent{name_postfix}'] = noise
-    st.session_state[f'image{name_postfix}'] = np.transpose(vutils.make_grid(generated, padding=2, normalize=True).cpu(), (1, 2, 0)).numpy()
-    st.session_state[f'features{name_postfix}'] = features
+  device = load_device()
+  generator = load_generator(device)
+  classifier = load_classifier(device)
+  dataset = load_dataset()
+  manipulator = load_manipulator(generator, classifier, device)
+  FeatureCount = len(dataset.annotations.columns)
 
-  def handle_manipulation(attribute: str, steps: int, alpha: float):
-    if attribute == NoFeature: return
+  def handle_generation():
+    vector = create_latent_vectors(1, LatentVectorSize, device)
 
-    attribute_index = list(dataset.annotations.columns).index(choice)
-    target_annotations = st.session_state['features'].copy()
-    target_annotations[attribute_index] = 1.0 if target_annotations[attribute_index] < FeatureThreshold else .0
+    generated = generator(vector)
+    features = classifier(generated)
 
-    vector, vectors = manipulator.manipulate(
-      st.session_state['latent'].clone(),
-      torch.tensor(target_annotations, device=device),
-      alpha=alpha,
-      steps=steps
-    )
-
-    figure = plt.figure(figsize=(8, 8))
-    plt.axis("off")
-
-    images = [
-      [
-        plt.imshow(
-          np.transpose(
-            vutils.make_grid(generator(torch.tensor(i, device=device)).cpu().squeeze().detach(), padding=2, normalize=True),
-            (1, 2, 0),
-          ),
-          animated=True
-        )
-      ] for i in vectors[::40]
-    ]
-
-    print(len(images), 'saving...')
-    frames = animation.ArtistAnimation(figure, images, interval=1000, repeat_delay=1000, blit=True)
-    frames.save('animation.gif', writer='imagemagick', fps=20)
-    print('saved')
-
-    generate_image(name_postfix="_manipulated", noise=vector)
-
-
-  manipulator = LatentSpaceManipulator(generator, classifier, device)
-  base = st.container()
-  target = st.container()
+    st.session_state[f'latent'] = vector
+    st.session_state[f'image'] = array_from_torch(generated)
+    st.session_state[f'features'] = features.cpu().detach().numpy()[0]
 
   if 'image' not in st.session_state:
-    generate_image()
-  if 'image_manipulated' not in st.session_state:
-    st.session_state['image_manipulated'] = None
+    handle_generation()
 
-  with base:
-    st.button("Generate new image", on_click=generate_image)
+  with st.container():
+    st.button("Generate new image", on_click=handle_generation)
     st.image(st.session_state['image'])
 
     st.write("Identified features:")
 
     columns = st.columns(4)
     rows = ceil(len(st.session_state['features']) / len(columns))
-
     for x in range(rows):
       for y, column in enumerate(columns):
         index = x * len(columns) + y
-        if index < len(dataset.annotations.columns):
-          column.checkbox(
-            dataset.annotations.columns[index],
-            value=st.session_state['features'][index] >= FeatureThreshold
-          )
+        if index > FeatureCount: break
+        column.checkbox(
+          dataset.annotations.columns[index],
+          value=st.session_state['features'][index] >= FeatureThreshold,
+          key=f'{dataset.annotations.columns[index]}',
+          disabled=True
+        )
 
-  with target:
-    choice = st.selectbox("Choose attribute to flip", [NoFeature, *dataset.annotations.columns])
+  with st.container():
+    NoFeature = "-"
+
+    choice = st.selectbox("Choose attribute to flip", (NoFeature, *dataset.annotations.columns))
     steps = st.slider("Number of steps", min_value=100, max_value=10_000, value=1000)
-    alpha = st.slider("Alpha", min_value=0.001, max_value=1.0, value=0.1)
+    alpha = st.slider("Alpha", min_value=0.001, max_value=1.0, value=0.5)
 
-    st.button("Manipulate", on_click=handle_manipulation, args=(choice, steps, alpha))
+    def handle_manipulation(attribute: str, steps: int, alpha: float):
+      index = dataset.annotations.columns.get_loc(attribute)
+      target_annotations = st.session_state['features']
+      target_annotations[index] = 1.0 if target_annotations[index] < FeatureThreshold else .0
 
-    if st.session_state['image_manipulated'] is not None:
+      features: np.ndarray
+      images: list[np.ndarray] = []
+      def record_steps(
+          step: int,
+          image: torch.Tensor,
+          traits: torch.Tensor,
+          _: torch.Tensor
+      ):
+        nonlocal features
+
+        if step % 40 == 0 or step == steps - 1:
+          images.append(array_from_torch(image))
+
+        if step != steps - 1:
+          features = traits.cpu().detach().numpy()[0]
+
+      vector = manipulator.manipulate(
+        st.session_state['latent'],
+        torch.tensor(target_annotations, device=device),
+        alpha=alpha,
+        steps=steps,
+        on_step=record_steps
+      )
+
+      figure = plt.figure(figsize=(8, 8))
+      plt.axis("off")
+      frames = [[plt.imshow(image, animated=True)] for image in images]
+      frames = animation.ArtistAnimation(figure, frames, interval=1000, repeat_delay=1000, blit=True)
+      frames.save('animation.gif', writer='imagemagick', fps=20)
+
+      st.session_state[f'latent_manipulated'] = vector
+      st.session_state[f'image_manipulated'] = images[0]
+      st.session_state[f'features_manipulated'] = features
+
+    st.button("Manipulate", on_click=handle_manipulation, args=(choice, steps, alpha), disabled=choice == NoFeature)
+
+    if 'image_manipulated' in st.session_state:
       st.image(st.session_state['image_manipulated'])
       st.image('animation.gif')
 
       st.write("Identified features:")
-      columns = st.columns(3)
-      rows = ceil(len(st.session_state['features_manipulated']) / len(columns))
 
+      columns = st.columns(4)
+      rows = ceil(len(st.session_state['features_manipulated']) / len(columns))
       for x in range(rows):
         for y, column in enumerate(columns):
           index = x * len(columns) + y
-          if index < len(dataset.annotations.columns):
-            column.checkbox(
-              dataset.annotations.columns[index],
-              value=st.session_state['features_manipulated'][index] >= FeatureThreshold,
-              key=f'{dataset.annotations.columns[index]}_manipulated'
-            )
-
+          if index > FeatureCount: break
+          column.checkbox(
+            dataset.annotations.columns[index],
+            value=st.session_state['features_manipulated'][index] >= FeatureThreshold,
+            key=f'{dataset.annotations.columns[index]}_manipulated',
+            disabled=True
+          )
 
 if __name__ == '__main__':
   main()
--- src/mod/models/manipulator.py
+++ src/mod/models/manipulator.py
@@ -1,5 +1,7 @@
+from typing import Callable, Optional
+
 import torch
-from torch import nn
+from torch import nn, Tensor
 
 class LatentSpaceManipulator:
   def __init__(self, generator: nn.Module, classifier: nn.Module, device: torch.device):
@@ -7,23 +9,31 @@ class LatentSpaceManipulator:
     self.classifier = classifier
     self.device = device
 
-  def manipulate(self, latent_vector, target_traits, alpha=0.01, steps=100):
-    self.generator.to(self.device)
-    self.classifier.to(self.device)
+  def manipulate(
+      self,
+      latent_vector: Tensor,
+      target_traits: Tensor,
+      alpha: float = 0.01,
+      steps: int = 100,
+      on_step: Optional[Callable[[int, Tensor, Tensor, Tensor], None]] = None
+  ):
     latent_vector = latent_vector.clone().detach().requires_grad_(True)
+    self.classifier.to(self.device)
+    self.generator.to(self.device)
     latent_vector.to(self.device)
     target_traits.to(self.device)
 
-    vectors = [latent_vector.clone()]
     for _ in range(steps):
       generated_image = self.generator(latent_vector)
       predicted_traits = self.classifier(generated_image)
+
       loss = ((predicted_traits - target_traits) ** 2).mean()
       loss.backward()
 
       with torch.no_grad():
         latent_vector -= alpha * latent_vector.grad
       latent_vector.grad.zero_()
-      vectors.append(latent_vector.clone())
 
-    return latent_vector, vectors
+      if on_step: on_step(_, generated_image, predicted_traits, latent_vector.clone())
+
+    return latent_vector
