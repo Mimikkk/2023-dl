efef1865f15b3fa539eb655506fd2c9302911038
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2024-01-24 12:46:21 +0100

    imports

--- src/mod/metrics/fid.py
+++ src/mod/metrics/fid.py
@@ -1,13 +1,13 @@
 from torchmetrics.image.fid import FrechetInceptionDistance
-import torch
+from torch import Tensor, uint8
 
 
-def calculate_fid(images_fake: torch.Tensor, images_real: torch.Tensor):
+def calculate_fid(images_fake: Tensor, images_real: Tensor):
   fid = FrechetInceptionDistance(feature=64)
 
   if images_fake.max() > 1.0 or images_real.max() > 1.0:
-    images_fake = (images_fake * 255).clamp(0, 255).to(torch.uint8)
-    images_real = (images_real * 255).clamp(0, 255).to(torch.uint8)
+    images_fake = (images_fake * 255).clamp(0, 255).to(uint8)
+    images_real = (images_real * 255).clamp(0, 255).to(uint8)
 
   fid.update(images_fake.to("cpu"), real=False)
   fid.update(images_real.to("cpu"), real=True)
